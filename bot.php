16442ff44d9da55fed347d8ea16c2be481aff4ed
Sasithorn <ann_sasithorn@hotmail.com>
2017-06-02 16:55:46 +0700

    commit

--- bot.php
+++ bot.php
@@ -73,7 +73,7 @@ if (!is_null($events['events'])) {
 					]
 				}						
 			}';
-			//$messages = json_decode($a3);
+			$messages = json_decode($a3);
 			$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
