5d2d781a56dd1f2cb5ce6c87accce276291f356c
Sasithorn <ann_sasithorn@hotmail.com>
2017-06-02 17:59:36 +0700

    commit

--- bot.php
+++ bot.php
@@ -9,98 +9,101 @@ $events = json_decode($content, true);
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-
-			// Build message to reply back
-			/*
+		// Get replyToken
+		$replyToken = $event['replyToken'];
+		
+		// Build message to reply back
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text' &&  $event['message']['text'] == 'hi')
+		{
 			// Reply with Text
 			$messages = [
 				'type' => 'text',
-				'text' => $text
-			];*/
-
+				'text' => 'หวัดดี'
+			];
+		}
+		else if($event['type'] == 'message' && $event['message']['type'] == 'sticker')
+		{
 			// Reply with Sticker
 			$messages = [
 				'type' => 'sticker',
 				'packageId' => '2',
 				'stickerId' => '145'
 			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$a3 = '
-			{
-  "type": "template",
-  "altText": "this is a carousel template",
-  "template": {
-      "type": "carousel",
-      "columns": [
-          {
-			"thumbnailImageUrl" : "https://www.advanced-media.co.jp/common/images/corporate/top_message/president.jpg",
-            "title": "Advance Media",
-            "text": "Communicate with machines as part of our everyday lives",
-            "actions": [
-                {
-                    "type": "uri",
-                    "label": "Mobile",
-                    "uri": "https://www.advanced-media.co.jp/english/solution/mobile"
-                },
-				{
-                    "type": "uri",
-                    "label": "Conference",
-                    "uri": "https://www.advanced-media.co.jp/english/solution/conferenceproceedings"
-                }
-            ]
-          },
-          {
-			"thumbnailImageUrl" : "https://www.advanced-media.co.jp/common/images/advm_contents_amivoice_images_strong1.png",
-            "title": "AmiVoice Thai",
-            "text": "Consulting services for all commercial enterprises",
-            "actions": [
-                {
-                    "type": "uri",
-                    "label": "Company Profile",
-                    "uri": "http://www.amivoicethai.com"
-                },
-				{
-                    "type": "uri",
-                    "label": "Logistic",
-                    "uri": "https://www.advanced-media.co.jp/english/solution/logistics"
-                }
-            ]
-          }
-      ]
-  }
-}';
-			$messages = json_decode($a3);
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-			
-			echo $post. "\r\n";
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			$curl_info = curl_getinfo($ch);
-			curl_close($ch);
-			
-			print_r($curl_info);
-
-			echo $result . "\r\n";
 		}
+		else
+		{
+			$ret = '
+			{
+			  "type": "template",
+			  "altText": "this is a carousel template",
+			  "template": {
+				  "type": "carousel",
+				  "columns": [
+					  {
+						"thumbnailImageUrl" : "https://www.advanced-media.co.jp/common/images/corporate/top_message/president.jpg",
+						"title": "Advance Media",
+						"text": "Communicate with machines as part of our everyday lives",
+						"actions": [
+							{
+								"type": "uri",
+								"label": "Mobile",
+								"uri": "https://www.advanced-media.co.jp/english/solution/mobile"
+							},
+							{
+								"type": "uri",
+								"label": "Conference",
+								"uri": "https://www.advanced-media.co.jp/english/solution/conferenceproceedings"
+							}
+						]
+					  },
+					  {
+						"thumbnailImageUrl" : "https://www.advanced-media.co.jp/common/images/advm_contents_amivoice_images_strong1.png",
+						"title": "AmiVoice Thai",
+						"text": "Consulting services for all commercial enterprises",
+						"actions": [
+							{
+								"type": "uri",
+								"label": "Company Profile",
+								"uri": "http://www.amivoicethai.com"
+							},
+							{
+								"type": "uri",
+								"label": "Logistic",
+								"uri": "https://www.advanced-media.co.jp/english/solution/logistics"
+							}
+						]
+					  }
+				  ]
+			  }
+			}';
+			$messages = json_decode($ret);
+		}
+
+		// Make a POST Request to Messaging API to reply to sender
+		$url = 'https://api.line.me/v2/bot/message/reply';
+		$data = [
+			'replyToken' => $replyToken,
+			'messages' => [$messages],
+		];
+		$post = json_encode($data);
+		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+		
+		echo $post. "\r\n";
+
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		$result = curl_exec($ch);
+		$curl_info = curl_getinfo($ch);
+		curl_close($ch);
+		
+		print_r($curl_info);
+
+		echo $result . "\r\n";
+		
 	}
 }
 echo "OK";
