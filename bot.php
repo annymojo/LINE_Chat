9666322188a247294fe331954cd17eac6f325b62
Sasithorn <ann_sasithorn@hotmail.com>
2017-06-02 16:51:57 +0700

    commit

--- bot.php
+++ bot.php
@@ -75,7 +75,7 @@ if (!is_null($events['events'])) {
 			//$messages = json_decode($a3);
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => $messages,
+				'messages' => [$messages],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
